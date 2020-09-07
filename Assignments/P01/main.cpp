f645e6164c239f967fd1e4c8e247f99b156feee2
Jon <18120090+jonhogan@users.noreply.github.com>
2020-09-07 16:43:16 -0500

    Add main.cpp

---
+++ Assignments/P01/main.cpp
@@ -0,0 +1,26 @@
+/*************************************
+ * Jonathan Hogan                    *
+ * Dr. Stringfellow                  *
+ * CMPS-2433-101                     *
+ * September 23 2020                 *
+ *                                   *
+ * **********************************/
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int HighBit (int);
+int NextHigh (int);
+
+int main ()
+{
+    ifstream InFile;
+    ofstream OutFile;
+
+    InFile.open("HighBits.dat");
+
+
+}

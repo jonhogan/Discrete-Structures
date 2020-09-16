9c8367bdb7cb244c07cff7ee238115c0348ab105
Jonathan <18120090+jonhogan@users.noreply.github.com>
2020-09-16 07:40:52 -0500

    Update main.cpp

--- Assignments/P01/main.cpp
+++ Assignments/P01/main.cpp
@@ -7,55 +7,95 @@
  * *******************************************************************/
 
 #include <iostream>
-#include <cmath>
 #include <fstream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
-int HighBit (unsigned int);
-int NextHigh (unsigned int, int);
+int HighBit(unsigned int);
+int NextHigh(unsigned int, int);
 
-int main ()
+int main()
 {
-    int counter, index;
-    unsigned int number;
+    int cases, index, k2;
+    unsigned int k;
+    string fileName;
 
-    ifstream InFile;
-    ofstream OutFile;
+    ifstream inFile;
+    ofstream outFile;
 
-    InFile.open("HighBits");
-    
-    InFile >> counter;
+    cout << "Please enter the name of the file you wish to use: ";
+    cin >> fileName;
 
-    for (int i =0; i < counter; i++)
+    inFile.open(fileName);
+    outFile.open("output.txt");
+
+    inFile >> cases;
+
+    outFile << "Jonathan Hogan\nHighest Bit Program 1\n\n"
+         << "There are " << cases << " test cases.\n\n"
+         << "k   HighestBit   k2\n___________________\n";
+
+    for (int i = 0; i < cases; i++)
     {
-        InFile >> number;
-        index = HighBit(number);
+        inFile >> k;
+        index = HighBit(k);
+        k2 = NextHigh(k, index);
+        if (index < 0)
+        {
+            outFile << setw(5) << k << setw(6) << '-' << setw(8)
+                << k2 << '\n';
+        }
+        else
+        {
+            outFile << setw(5) << k << setw(6) << index << setw(8) 
+                << k2 << '\n';
+        }
     }
 
+    inFile.close();
+    outFile.close();
 
+    return 0;
 }
 
 
 // Function to locate the index of the highest bit.
-int HighBit(unsigned int a) {
+int HighBit(unsigned int a)
+{
     int index = 1;
-
+    
     while (a != 1)
     {
         if (a == 0)
         {
             index = -1;
+            break;
         }
-        a = a >> 1;
-        index++;
+        else
+        {
+            a = a >> 1;
+            index++;
+        }
     }
-
+    
     return index;
 }
 
-// Function used to mask the next highest bit to 1
-int NextHigh(unsigned int a, int b) {
-    return 0,
+// Function used to mask bit to the left to 1
+int NextHigh(unsigned int a, int b)
+{
+    int orNum, orHigh;
+    
+    if (b < 0)
+    {
+        orNum = 1 << 0;
+    }
+    else
+    {
+        orNum = 1 << b;
+    }
+    orHigh = a | orNum;
+    return orHigh;
 }
